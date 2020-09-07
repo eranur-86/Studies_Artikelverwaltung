daf66cfed3f4a5d8c1d00ace1b526b8669db972f
Thomas <70510595+eranur-86@users.noreply.github.com>
2020-09-07 11:34:32 +0200

    Updated getter / setter

--- src/java/Articles/Artikelspeicher.java
+++ src/java/Articles/Artikelspeicher.java
@@ -11,63 +11,26 @@ import javax.faces.bean.RequestScoped;
 @RequestScoped
 public class Artikelspeicher {
     
-    private String vorname;
-    private String nachname;
-    private String strasse;
-    private int hausnummer;
-    private String postleitzahl;
-    private String ort;
+    private String name;
+    private String artikelnummer;
 
-    public String getVorname() {
-        return vorname;
+    public String getName() {
+        return name;
     }
 
-    public void setVorname(String vorname) {
-        this.vorname = vorname;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getNachname() {
-        return nachname;
+    public String getArtikelnummer() {
+        return artikelnummer;
     }
 
-    public void setNachname(String nachname) {
-        this.nachname = nachname;
-    }
-
-    public String getStrasse() {
-        return strasse;
-    }
-
-    public void setStrasse(String strasse) {
-        this.strasse = strasse;
-    }
-
-    public int getHausnummer() {
-        return hausnummer;
-    }
-
-    public void setHausnummer(int hausnummer) {
-        this.hausnummer = hausnummer;
-    }
-
-    public String getPostleitzahl() {
-        return postleitzahl;
-    }
-
-    public void setPostleitzahl(String postleitzahl) {
-        this.postleitzahl = postleitzahl;
-    }
-
-    public String getOrt() {
-        return ort;
-    }
-
-    public void setOrt(String ort) {
-        this.ort = ort;
+    public void setArtikelnummer(String artikelnummer) {
+        this.artikelnummer = artikelnummer;
     }
     
-
-
+    
 
     /**
      * Creates a new instance of Artikelspeicher
